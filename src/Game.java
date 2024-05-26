5e6182691b6491ea4c5bc5533d2d5cfe2601673b
Everblade <jasonherman.jh@gmail.com>
2024-05-26 17:18:46 +0800

    added difficulty option if the user plays and add random move for monster

--- src/Game.java
+++ src/Game.java
@@ -4,19 +4,19 @@ public class Game {
 
     // portable newline
     private final static String NEWLINE = System.getProperty("line.separator");
-    private static Scanner kb = new Scanner (System.in);
-  
+    private static Scanner kb = new Scanner(System.in);
+
     private Dungeon dungeon;     // the dungeon
     private char MONSTER;        // name of the monster (A - Z)
     private char ROGUE = '@';    // name of the rogue
     private int N;               // board dimension
     private Site monsterSite;    // location of monster
     private Site rogueSite;      // location of rogue
-    private Monster monster;    // the monster
+    private Monster monster;     // the monster
     private Rogue rogue;         // the rogue
 
     // initialize board from file
-    public Game(In in) {
+    public Game(In in, boolean monsterEasy, boolean rogueEasy) {
 
         // read in data
         N = Integer.parseInt(in.readLine());
@@ -24,7 +24,7 @@ public class Game {
         for (int i = 0; i < N; i++) {
             String s = in.readLine();
             for (int j = 0; j < N; j++) {
-                board[i][j] = s.charAt(2*j);
+                board[i][j] = s.charAt(2 * j);
 
                 // check for monster's location
                 if (board[i][j] >= 'A' && board[i][j] <= 'Z') {
@@ -36,105 +36,109 @@ public class Game {
                 // check for rogue's location
                 if (board[i][j] == ROGUE) {
                     board[i][j] = '.';
-                    rogueSite  = new Site(i, j);
+                    rogueSite = new Site(i, j);
                 }
             }
         }
         dungeon = new Dungeon(board);
-        monster = new Monster(this);
-        rogue   = new Rogue(this);
+        monster = new Monster(this, monsterEasy);
+        rogue = new Rogue(this, rogueEasy);
     }
 
     // return position of monster and rogue
-    public Site getMonsterSite() { return monsterSite; }
-    public Site getRogueSite()   { return rogueSite;   }
-    public Dungeon getDungeon()  { return dungeon;     }
+    public Site getMonsterSite() {
+        return monsterSite;
+    }
+
+    public Site getRogueSite() {
+        return rogueSite;
+    }
+
+    public Dungeon getDungeon() {
+        return dungeon;
+    }
 
     // Clear The Terminal Screen
-    public static void clearDisplay(){
+    public static void clearDisplay() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
     // play until monster catches the rogue
-    public void play() {
-        int option = gameModeSelection();
+    public void play(int option, boolean monsterEasy, boolean rogueEasy) {
         switch (option) {
             case 1:
-            for (int t = 1; true; t++) {
-                System.out.println("Move " + t);
-                System.out.println();
-                
-                // monster moves
-                if (monsterSite.equals(rogueSite)) break;
-                Site next = monster.userMove();
-                if (dungeon.isLegalMove(monsterSite, next)) monsterSite = next;
-                else throw new RuntimeException("Monster caught cheating");
-                System.out.println(this);
-    
-                // rogue moves
-                if (monsterSite.equals(rogueSite)) break;
-                next = rogue.move();
-                if (dungeon.isLegalMove(rogueSite, next)) rogueSite = next;
-                else throw new RuntimeException("Rogue caught cheating");
-                System.out.println(this);
-            }
-    
-            System.out.println("Caught by monster");
+                for (int t = 1; true; t++) {
+                    System.out.println("Move " + t);
+                    System.out.println();
+
+                    // monster moves
+                    if (monsterSite.equals(rogueSite)) break;
+                    Site next = monster.userMove();
+                    if (dungeon.isLegalMove(monsterSite, next)) monsterSite = next;
+                    else throw new RuntimeException("Monster caught cheating");
+                    System.out.println(this);
+
+                    // rogue moves
+                    if (monsterSite.equals(rogueSite)) break;
+                    next = rogue.move();
+                    if (dungeon.isLegalMove(rogueSite, next)) rogueSite = next;
+                    else throw new RuntimeException("Rogue caught cheating");
+                    System.out.println(this);
+                }
+
+                System.out.println("Caught by monster");
                 break;
             case 2:
-            for (int t = 1; true; t++) {
-                System.out.println("Move " + t);
-                System.out.println();
-                
-                // monster moves
-                if (monsterSite.equals(rogueSite)) break;
-                Site next = monster.move();
-                if (dungeon.isLegalMove(monsterSite, next)) monsterSite = next;
-                else throw new RuntimeException("Monster caught cheating");
-                System.out.println(this);
-    
-                // rogue moves
-                if (monsterSite.equals(rogueSite)) break;
-                next = rogue.userMove();
-                if (dungeon.isLegalMove(rogueSite, next)) rogueSite = next;
-                else throw new RuntimeException("Rogue caught cheating");
-                System.out.println(this);
-            }
-    
-            System.out.println("Caught by monster");
+                for (int t = 1; true; t++) {
+                    System.out.println("Move " + t);
+                    System.out.println();
+
+                    // monster moves
+                    if (monsterSite.equals(rogueSite)) break;
+                    Site next = monster.move();
+                    if (dungeon.isLegalMove(monsterSite, next)) monsterSite = next;
+                    else throw new RuntimeException("Monster caught cheating");
+                    System.out.println(this);
+
+                    // rogue moves
+                    if (monsterSite.equals(rogueSite)) break;
+                    next = rogue.userMove();
+                    if (dungeon.isLegalMove(rogueSite, next)) rogueSite = next;
+                    else throw new RuntimeException("Rogue caught cheating");
+                    System.out.println(this);
+                }
+
+                System.out.println("Caught by monster");
                 break;
 
             case 3:
-            for (int t = 1; true; t++) {
-                System.out.println("Move " + t);
-                System.out.println();
-                
-                // monster moves
-                if (monsterSite.equals(rogueSite)) break;
-                Site next = monster.move();
-                if (dungeon.isLegalMove(monsterSite, next)) monsterSite = next;
-                else throw new RuntimeException("Monster caught cheating");
-                System.out.println(this);
-    
-                // rogue moves
-                if (monsterSite.equals(rogueSite)) break;
-                next = rogue.move();
-                if (dungeon.isLegalMove(rogueSite, next)) rogueSite = next;
-                else throw new RuntimeException("Rogue caught cheating");
-                System.out.println(this);
-            }
-    
-            System.out.println("Caught by monster");
+                for (int t = 1; true; t++) {
+                    System.out.println("Move " + t);
+                    System.out.println();
+
+                    // monster moves
+                    if (monsterSite.equals(rogueSite)) break;
+                    Site next = monster.move();
+                    if (dungeon.isLegalMove(monsterSite, next)) monsterSite = next;
+                    else throw new RuntimeException("Monster caught cheating");
+                    System.out.println(this);
+
+                    // rogue moves
+                    if (monsterSite.equals(rogueSite)) break;
+                    next = rogue.move();
+                    if (dungeon.isLegalMove(rogueSite, next)) rogueSite = next;
+                    else throw new RuntimeException("Rogue caught cheating");
+                    System.out.println(this);
+                }
+
+                System.out.println("Caught by monster");
                 break;
             default:
                 System.out.println("Game is Corrupted, Please Restart The Game!");
                 break;
         }
-        
-
     }
-
 
     // string representation of game state (inefficient because of Site and string concat)
     public String toString() {
@@ -143,19 +147,20 @@ public class Game {
             for (int j = 0; j < N; j++) {
                 Site site = new Site(i, j);
                 if (rogueSite.equals(monsterSite) && (rogueSite.equals(site))) s += "* ";
-                else if (rogueSite.equals(site))                               s += ROGUE   + " ";
-                else if (monsterSite.equals(site))                             s += MONSTER + " ";
-                else if (dungeon.isRoom(site))                                 s += ". ";
-                else if (dungeon.isCorridor(site))                             s += "+ ";
-                else if (dungeon.isRoom(site))                                 s += ". ";
-                else if (dungeon.isWall(site))                                 s += "  ";
+                else if (rogueSite.equals(site)) s += ROGUE + " ";
+                else if (monsterSite.equals(site)) s += MONSTER + " ";
+                else if (dungeon.isRoom(site)) s += ". ";
+                else if (dungeon.isCorridor(site)) s += "+ ";
+                else if (dungeon.isRoom(site)) s += ". ";
+                else if (dungeon.isWall(site)) s += "  ";
             }
             s += NEWLINE;
         }
         return s;
     }
+
     //Displays Welcome Messages to User
-    private static void welcomeDisplay(){
+    private static void welcomeDisplay() {
         clearDisplay();
         System.out.println("++++++++++++++++++++++++++++");
         System.out.println("++       WELCOME TO       ++");
@@ -164,13 +169,13 @@ public class Game {
         System.out.println("++++++++++++++++++++++++++++");
         System.out.println("\nPRESS ENTER TO START!");
         String enter = kb.nextLine();
-        if(enter != null){
+        if (enter != null) {
             clearDisplay();
         }
     }
 
     //Interface to Choose Dungeon
-    private static char chooseDungeon(){
+    private static char chooseDungeon() {
         System.out.println("Please Choose Dungeon from A to R: ");
         String dungeon = "";
         boolean validInput = false;
@@ -187,8 +192,8 @@ public class Game {
         return dungeon.charAt(0);
     }
 
-    private static int gameModeSelection(){
-        String gameMode = ""; 
+    private static int gameModeSelection() {
+        String gameMode = "";
         boolean validInput = false;
         int option = 0;
 
@@ -211,28 +216,66 @@ public class Game {
         return option;
     }
 
-    public static void moveInstruction(){
+    private static boolean difficultySelection() {
+        String difficulty = "";
+        boolean validInput = false;
+        boolean isEasy = false;
+
+        System.out.println("Please Select a Difficulty Level: ");
+        System.out.println("1. Easy");
+        System.out.println("2. Difficult");
+
+        while (!validInput) {
+            System.out.print("Enter a number (1-2): ");
+            difficulty = kb.nextLine();
+            if (difficulty.equals("1")) {
+                isEasy = true;
+                validInput = true;
+            } else if (difficulty.equals("2")) {
+                isEasy = false;
+                validInput = true;
+            } else {
+                System.out.println("Invalid option. Please input the correct option (1-2).");
+            }
+        }
+
+        return isEasy;
+    }
+
+    public static void moveInstruction() {
         System.out.println("When Playing As Monster or Rogue");
         System.out.println("Please Use Your Keyboard and Beware Of Walls");
         System.out.println("You May Move In Any Directions You Want When You Are In A Room");
         System.out.println("You May Only Move To only 4 Different Directions When You Are In Corridor");
         System.out.println("Directions: ");
-        System.out.println("Q = North West, W = North, E = North East, A = West, S = Stay, D = East, Z = South West, X = South, C = South East");
-        System.out.println("If You Hit A Wall You Will Remain In The Same Position Until The Next Move");
-        System.out.println("Good luck and Enjoy The Game!");
-        System.out.println("Press Enter to continue...");
-        kb.nextLine();
-        clearDisplay();
+        System.out.println("Q = North West, W = North, E = North East");
+        System.out.println("A = West, S = South, D = East");
+        System.out.println("Z = South West, X = South, C = South East");
+        System.out.println("Press Any Key to Continue...");
+        String enter = kb.nextLine();
+        if (enter != null) {
+            clearDisplay();
+        }
     }
-
 
     public static void main(String[] args) {
         welcomeDisplay();
-        In stdin = new In("./Dungeons/dungeon"+chooseDungeon()+".txt");
-        Game game = new Game(stdin);
-        System.out.println("The Dungeon You Selected Is \n\n" + game);
-        game.play();
+        char dungeon = chooseDungeon();
+        In stdin = new In("./Dungeons/dungeon" + dungeon + ".txt");
+        moveInstruction();
 
+        int option = gameModeSelection();
+        boolean monsterEasy = false;
+        boolean rogueEasy = false;
+
+        if (option == 1) {
+            rogueEasy = difficultySelection();
+        } else if (option == 2) {
+            monsterEasy = difficultySelection();
+        }
+
+        Game game = new Game(stdin, monsterEasy, rogueEasy);
+        System.out.println(game);
+        game.play(option, monsterEasy, rogueEasy);
     }
-
 }
--- src/Rogue.java
+++ src/Rogue.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Rogue {
     private Game game;
     private Dungeon dungeon;
+    private boolean isEasy;
     private int N;
     private Scanner kb = new Scanner(System.in);
     private static Map<String, int[]> directionMap = new HashMap<>();
@@ -19,14 +20,40 @@ public class Rogue {
         directionMap.put("C", new int[]{1, 1});   // South East
     }
 
-    public Rogue(Game game) {
+    public Rogue(Game game, boolean isEasy) {
         this.game = game;
         this.dungeon = game.getDungeon();
+        this.isEasy = isEasy;
         this.N = dungeon.size();
     }
 
     // Return the next move for the rogue
     public Site move() {
+        if (isEasy) {
+            return randomMove();
+        } else {
+            return intelligentMove();
+        }
+    }
+
+    // Choose a random legal move
+    private Site randomMove() {
+        Site rogueSite = game.getRogueSite();
+        List<Site> neighbors = getNeighbors(rogueSite);
+        List<Site> legalMoves = new ArrayList<>();
+        for (Site neighbor : neighbors) {
+            if (dungeon.isLegalMove(rogueSite, neighbor)) {
+                legalMoves.add(neighbor);
+            }
+        }
+        if (legalMoves.isEmpty()) {
+            return rogueSite; // Stay in place if no move is possible
+        }
+        return legalMoves.get((int) (Math.random() * legalMoves.size()));
+    }
+
+    // Intelligent move logic for difficult difficulty
+    private Site intelligentMove() {
         Site rogue = game.getRogueSite();
         Site monster = game.getMonsterSite();
 
@@ -110,22 +137,7 @@ public class Rogue {
             }
         }
 
-        return bestMove != null ? bestMove : randomMove(rogue);
-    }
-
-    // Choose a random legal move
-    private Site randomMove(Site rogue) {
-        List<Site> neighbors = getNeighbors(rogue);
-        List<Site> legalMoves = new ArrayList<>();
-        for (Site neighbor : neighbors) {
-            if (dungeon.isLegalMove(rogue, neighbor)) {
-                legalMoves.add(neighbor);
-            }
-        }
-        if (legalMoves.isEmpty()) {
-            return rogue; // Stay in place if no move is possible
-        }
-        return legalMoves.get((int) (Math.random() * legalMoves.size()));
+        return bestMove != null ? bestMove : rogue;
     }
 
     private List<Site> getNeighbors(Site site) {
@@ -140,7 +152,8 @@ public class Rogue {
         }
         return neighbors;
     }
-    public Site userMove(){
+
+    public Site userMove() {
         System.out.println("Enter your move (Q, W, E, A, S, D, Z, X, C):");
         String move;
         int[] directions;
@@ -155,9 +168,9 @@ public class Rogue {
             }
         }
 
-        Site currentSite = game.getRogueSite();  // Assuming you are moving a monster
+        Site currentSite = game.getRogueSite();
         Site userMove = new Site(currentSite.i() + directions[0], currentSite.j() + directions[1]);
-        if(dungeon.isLegalMove(currentSite, userMove)==true){
+        if (dungeon.isLegalMove(currentSite, userMove)) {
             return userMove;
         }
         System.out.println("Oops! You Hit A Wall");
